877b751f8c2fd7fc4d2bd2bb7fccd52a88f64ad4
Tomislav Pesut <tomca32@gmail.com>
2015-11-01 18:39:12 -0500

    Add fields to Student table

--- db/migrate/20151101232844_create_students.rb
+++ db/migrate/20151101232844_create_students.rb
@@ -1,6 +1,9 @@
 class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.date :dob, null: false
 
       t.timestamps null: false
     end
