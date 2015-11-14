a03f1b9b7f6964afa382a30e7565ea5d3b7b8e81
John Barker <jebarker@gmail.com>
2015-11-14 13:42:33 -0500

    Added student list view and tests

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,14 +1,15 @@
 class StudentsController < ApplicationController
   def new
-	@student = Student.new
+    @students = Student.all
+    @student = Student.new
   end
 
   def create
-  	@student = Student.new(params.require(:student).permit(:first_name, :last_name, :dob))
-  	if @student.save
-  		redirect_to :new_student
-  	else
-  		render :new
-  	end
+    @student = Student.new(params.require(:student).permit(:first_name, :last_name, :dob))
+    if @student.save
+      redirect_to :new_student
+    else
+      render :new
+    end
   end
 end
--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class StudentsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create a student when supplied valid params" do
+    post :create, student: {
+      first_name: 'Trevor',
+      last_name: 'Noah',
+      "dob(1i)" => "2015", "dob(2i)" => "11", "dob(3i)" => 17}
+    assert_redirected_to :new_student
+
+    student = Student.last
+    assert_equal student.first_name, 'Trevor'
+    assert_equal student.last_name, 'Noah'
+  end
+
+  test "new should have a list of students" do
+    get :new
+    assert_response :success
+
+    assert_includes assigns(:students), students(:tomislav)
+  end
 end
