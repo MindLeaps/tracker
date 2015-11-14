d01b9f2f50bdf4e34b51b79a88c277688b04aa29
John Barker <jebarker@gmail.com>
2015-11-14 14:42:04 -0500

    Move completely to rspec

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe StudentsController, type: :controller do
+  fixtures :students
+  
+  describe '#new' do
+    it 'create a student when supplied valid params' do
+      post :create, student: {
+        first_name: 'Trevor',
+        last_name: 'Noah',
+        "dob(1i)" => "2015", "dob(2i)" => "11", "dob(3i)" => 17}
+      assert_response :success
 
+      student = Student.last
+      assert_equal student.first_name, 'Trevor'
+      assert_equal student.last_name, 'Noah'
+    end
+  end
+
+  describe '#index' do
+    it 'gets a list of students' do
+      get :index
+      assert_response :success
+
+      assert_includes assigns(:students), students(:tomislav)
+    end
+  end
 end
