d7f6211d65de26aa55d3519b21f3877a3387e490
Tomislav Pesut <tomca32@gmail.com>
2015-12-20 14:54:01 -0500

    TRACK-1 - Added a default true value for esimated_dob

--- db/migrate/20151220030058_add_estimated_dob_to_students.rb
+++ db/migrate/20151220030058_add_estimated_dob_to_students.rb
@@ -1,7 +1,5 @@
 class AddEstimatedDobToStudents < ActiveRecord::Migration
   def change
-    add_column :students, :estimated_dob, :boolean
-    change_column :students, :estimated_dob, :boolean, null: false #This is because of an SQLite bug which won't lett us add a column with a NOT NULL constraint
-    # http://stackoverflow.com/questions/3170634/how-to-solve-cannot-add-a-not-null-column-with-default-value-null-in-sqlite3
+    add_column :students, :estimated_dob, :boolean, null: false, default: true
   end
 end
