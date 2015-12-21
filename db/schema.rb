859a64296ea944e5377f77f0941c3b5d5f6ecbbb
Tomislav Pesut <tomca32@gmail.com>
2015-12-20 21:11:11 -0500

    TRACK-2 created group resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151220030058) do
+ActiveRecord::Schema.define(version: 20151221020928) do
+
+  create_table "groups", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "students", force: :cascade do |t|
     t.string   "first_name",    null: false
