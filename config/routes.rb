a47eee477a5864f705fc12464723ad82eb033f99
Tomislav Pesut <tomca32@gmail.com>
2016-07-13 22:09:28 -0400

    TRACK-10 - Added home screen

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :groups, only: [:index, :create]
   resources :students, only: [:index, :create, :show, :edit, :update]
 
-  root to: 'students#index'
+  root to: 'home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
