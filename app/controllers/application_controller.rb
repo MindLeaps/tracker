ab01fa3529074da9c1b8a59e059b0f449c2f5f21
Tomislav Pesut <tomca32@gmail.com>
2016-07-19 23:20:16 -0400

    TRACK-10 - authenticating user before actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :authenticate_user
 
   private
 
   def current_user
     @current_user ||= User.find session[:user_id] if session[:user_id]
+  end
+
+  def authenticate_user
+    redirect_to root_url unless current_user
   end
 
   helper_method :current_user
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,4 +1,5 @@
 class StudentsController < ApplicationController
+  before_action :authenticate_user
   before_action do
     @students = Student.all
   end
