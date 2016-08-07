3a3b60d9c417f069b48052cd442553ae0efa5c87
Tomislav Pesut <tomca32@gmail.com>
2016-08-07 00:29:14 -0400

    TRACK-10 - Added Omniauth.config.full_host property in production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
   provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], prompt: :select_account
+  OmniAuth.config.full_host = "https://#{ENV['DOMAIN']}" if Rails.env.production?
 end
