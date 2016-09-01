3389a6cba38f8be0041b6e7db34fe2a477feaf3b
Tomislav Pesut <tomca32@gmail.com>
2016-09-01 11:23:50 -0400

    TRACK-25 - Showing gender in student list

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ActiveRecord::Base
   validates :first_name, :last_name, :dob, :gender, presence: true
   belongs_to :group
-  enum gender: { male: 0, female: 1 }
+  enum gender: { M: 0, F: 1 }
 
   delegate :group_name, to: :group, allow_nil: true
 end
