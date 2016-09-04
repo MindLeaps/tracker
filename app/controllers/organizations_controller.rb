034b083e26c00c25cc5e77c958aeef6e19703ca3
Tomislav Pesut <tomca32@gmail.com>
2016-09-04 15:00:05 -0400

    TRACK-9 - Listing and Creating organizations

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,2 +1,12 @@
 class OrganizationsController < ApplicationController
+  def index
+    @organizations = Organization.all
+    @organization = Organization.new
+  end
+
+  def create
+    @organization = Organization.new(params.require(:organization).permit(:organization_name))
+    @organization.save
+    redirect_to organizations_url
+  end
 end
