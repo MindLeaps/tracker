69f16dfc9a72fb6e92c5cfa4b0a96aa50e1b2812
Tomislav Pesut <tomca32@gmail.com>
2016-09-05 15:27:32 -0400

    TRACK-11 - Listing users

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     resources :students, only: [:index, :show]
   end
 
+  resources :users, only: [:index]
   resources :organizations, only: [:index, :create, :show]
   resources :chapters, only: [:index, :create, :show]
   resources :groups, only: [:index, :create, :show]
