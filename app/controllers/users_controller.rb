1b6a29746ce68f54b67e980e319a1608136ff82f
Tomislav Pesut <tomca32@gmail.com>
2016-09-05 20:32:56 -0400

    TRACK-32 - Authorizing User resource with super_admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,12 @@ class UsersController < ApplicationController
   end
 
   def index
+    authorize User
     @user = User.new
   end
 
   def create
+    authorize User
     @user = User.new params.require(:user).permit(:email)
     return redirect_to users_url if @user.save
     render :index
