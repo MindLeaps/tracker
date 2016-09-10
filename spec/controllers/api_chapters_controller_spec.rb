a13b26b778b07e0d25604f83b9d9cf1ed05f0b8d
Tomislav Pesut <tomca32@gmail.com>
2016-09-10 18:37:30 -0400

    Refactored API spec with Factory Girl

--- spec/controllers/api_chapters_controller_spec.rb
+++ spec/controllers/api_chapters_controller_spec.rb
@@ -1,28 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Api::ChaptersController, type: :controller do
-  fixtures :groups
-  fixtures :students
-  fixtures :chapters
   let(:json) { JSON.parse(response.body) }
-  let(:kigali_chapter) { chapters(:kigali_chapter) }
-  let(:group_ids) { [groups(:group_a), groups(:group_b)].map(&:id) }
+  before :all do
+    @chapter1 = create :chapter, chapter_name: 'Chapter Api Test Chapter One'
+    @chapter2 = create :chapter, chapter_name: 'Chapter Api Test Chapter Two'
+    @chapter3 = create :chapter, chapter_name: 'Chapter Api Test Chapter Three'
+
+    @group1 = create :group, chapter: @chapter1
+    @group2 = create :group, chapter: @chapter1
+  end
 
   describe '#index' do
     it 'gets a list of chapters' do
       get :index, format: :json
       expect(response).to be_success
-      expect(json.map { |g| g['chapter_name'] }).to include 'Kigali'
-      expect(json[0]['groups'].map { |g| g['group_name'] }).to include groups(:group_a).group_name
+      expect(json.map { |g| g['chapter_name'] }).to include 'Chapter Api Test Chapter One', 'Chapter Api Test Chapter Two', 'Chapter Api Test Chapter Three'
     end
   end
 
   describe '#show' do
     it 'gets a single chapter' do
-      get :show, params: { id: kigali_chapter.id }, format: :json
+      get :show, params: { id: @chapter1.id }, format: :json
       expect(response).to be_success
-      expect(json['chapter_name']).to eq kigali_chapter.chapter_name
-      expect(json['groups'].map { |g| g['id'] }).to include(*group_ids)
+      expect(json['chapter_name']).to eq @chapter1.chapter_name
+      expect(json['groups'].map { |g| g['id'] }).to include(@group1.id, @group2.id)
     end
   end
 end
