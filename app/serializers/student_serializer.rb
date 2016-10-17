7c5e1ab48add30ab870015e9ada8b42f2db8b84b
Tomislav Pesut <tomca32@gmail.com>
2016-10-17 17:41:17 -0400

    TRACK-58 - Namespaced student responses using a serializer

---
+++ app/serializers/student_serializer.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+class StudentSerializer < ActiveModel::Serializer
+  attributes :id, :first_name, :last_name, :dob, :estimated_dob, :group_id, :gender, :quartier, :guardian_name, :guardian_occupation,
+             :guardian_contact, :family_members, :health_insurance, :health_issues, :hiv_tested, :name_of_school, :school_level_completed,
+             :year_of_dropout, :reason_for_leaving, :notes, :organization_id
+end
--- spec/controllers/api_students_controller_spec.rb
+++ spec/controllers/api_students_controller_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 
 RSpec.describe Api::StudentsController, type: :controller do
   let(:json) { JSON.parse(response.body) }
+  let(:student) { JSON.parse(response.body)['student'] }
+  let(:students) { JSON.parse(response.body)['students'] }
 
   describe '#index' do
     before :each do
@@ -12,20 +14,37 @@ RSpec.describe Api::StudentsController, type: :controller do
       create :student, first_name: 'Spector', group: @test_group
       create :student, first_name: 'Another'
       create :student, first_name: 'Someone'
+
+      get :index, format: :json
     end
 
-    it 'gets a list of students' do
-      get :index, format: :json
+    it { should respond_with 200 }
 
-      expect(response).to be_success
-      expect(json.map { |s| s['first_name'] }).to include 'Api', 'Controller', 'Spector', 'Another', 'Someone'
+    it 'gets a list of students' do
+      expect(students.map { |s| s['first_name'] }).to include 'Api', 'Controller', 'Spector', 'Another', 'Someone'
     end
 
     it 'get only students belonging to Api Controller Test Group' do
       get :index, params: { group_id: @test_group.id }, format: :json
 
-      expect(json.count).to eq 3
-      expect(json.map { |s| s['first_name'] }).to include 'Api', 'Controller', 'Spector'
+      expect(students.count).to eq 3
+      expect(students.map { |s| s['first_name'] }).to include 'Api', 'Controller', 'Spector'
+    end
+  end
+
+  describe '#show' do
+    before :each do
+      @student = create :student
+
+      get :show, format: :json, params: { id: @student.id }
+    end
+
+    it { should respond_with 200 }
+
+    it 'responds with a specific student' do
+      expect(student['first_name']).to eq @student.first_name
+      expect(student['last_name']).to eq @student.last_name
+      expect(student['id']).to eq @student.id
     end
   end
 end
