35898e63c746d761ea776646cbbd2e26156910b7
Tomislav Pesut <tomca32@gmail.com>
2016-11-17 18:28:51 -0500

    TRACK-26 - Rubocop fixes

--- db/migrate/20161116033304_change_image_to_filename_in_student_images.rb
+++ db/migrate/20161116033304_change_image_to_filename_in_student_images.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class ChangeImageToFilenameInStudentImages < ActiveRecord::Migration[5.0]
   def change
     rename_column :student_images, :image, :filename
--- db/migrate/20161116035756_add_deleted_at_to_student_images.rb
+++ db/migrate/20161116035756_add_deleted_at_to_student_images.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class AddDeletedAtToStudentImages < ActiveRecord::Migration[5.0]
   def change
     add_column :student_images, :deleted_at, :datetime
