8df664427ffa0d2b59350a2ab3ab3c657df659f6
Tomislav Pesut <tomca32@gmail.com>
2016-11-20 18:48:41 -0500

    TRACK-26 - Added medium image size as 250px

--- app/uploaders/student_image_uploader.rb
+++ app/uploaders/student_image_uploader.rb
@@ -2,7 +2,7 @@
 class StudentImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -30,9 +30,9 @@ class StudentImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :medium do
+    process resize_to_fill: [250, 250]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
