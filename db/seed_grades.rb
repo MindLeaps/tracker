d32907ed9fc47e0010b62a2712f705ddab4d5738
Tomislav Pesut <tomca32@gmail.com>
2017-01-15 23:19:20 -0500

    TRACK-103 - Added seeding absences

--- db/seed_grades.rb
+++ db/seed_grades.rb
@@ -1,13 +1,4 @@
 # frozen_string_literal: true
-# rubocop:disable Style/IndentArray
-def seed_student_grades(student, lesson)
-  lesson.subject.skills.each do |skill|
-    student.grades.create([
-      { lesson: lesson, student: student, grade_descriptor: skill.grade_descriptors.sample }
-    ])
-  end
-end
-
 def seed_group_grades(group, subject)
   current_date = 6.months.ago
 
@@ -15,9 +6,25 @@ def seed_group_grades(group, subject)
     lesson = Lesson.create subject: subject, date: current_date, group: group
 
     group.students.each do |student|
-      seed_student_grades student, lesson
+      seed_student_performance student, lesson
     end
 
     current_date += 1.day
+  end
+end
+
+def seed_student_performance(student, lesson)
+  return mark_absent(student, lesson) if rand < 0.1
+
+  seed_student_grades student, lesson
+end
+
+def mark_absent(student, lesson)
+  lesson.mark_student_as_absent student
+end
+
+def seed_student_grades(student, lesson)
+  lesson.subject.skills.each do |skill|
+    student.grades.create lesson: lesson, student: student, grade_descriptor: skill.grade_descriptors.sample
   end
 end
