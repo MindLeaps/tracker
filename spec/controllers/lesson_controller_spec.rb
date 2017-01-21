7c2d6e8f6c4d1f177b0d248ab3ff182084ad632e
Tomislav Pesut <tomca32@gmail.com>
2017-01-21 15:10:09 -0500

    TRACK-104 - Not displaying deleted students in lesson view

--- spec/controllers/lesson_controller_spec.rb
+++ spec/controllers/lesson_controller_spec.rb
@@ -27,12 +27,22 @@ RSpec.describe LessonsController, type: :controller do
 
     describe '#show' do
       before :each do
-        lesson = create :lesson
+        @group = create :group
+        @active_student1 = create :student, group: @group
+        @active_student2 = create :student, group: @group
+        @deleted_student = create :student, group: @group, deleted_at: Time.zone.now
+
+        lesson = create :lesson, group: @group
         get :show, params: { id: lesson.id }
       end
 
       it { should respond_with 200 }
       it { should render_template 'show' }
+
+      it 'exposes non-deleted students from the lesson\'s group' do
+        expect(assigns(:students)).to include @active_student1, @active_student2
+        expect(assigns(:students)).not_to include @deleted_student
+      end
     end
 
     describe '#create' do
