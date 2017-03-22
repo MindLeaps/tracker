fb3a8c3e381c202c92482408585cae97190958dd
Tomislav Pesut <tomca32@gmail.com>
2017-03-21 22:27:03 -0400

    TRACK-38 Moved assignments controller spec to assignment request spec

--- spec/controllers/api/assignments_controller_spec.rb
+++ spec/controllers/api/assignments_controller_spec.rb
@@ -2,8 +2,6 @@
 require 'rails_helper'
 
 RSpec.describe Api::AssignmentsController, type: :controller do
-  let(:assignments) { JSON.parse(response.body)['assignments'] }
-  let(:assignment) { JSON.parse(response.body)['assignment'] }
   let(:admin) { create :admin }
 
   before :each do
@@ -12,42 +10,11 @@ RSpec.describe Api::AssignmentsController, type: :controller do
 
   describe '#index' do
     before :each do
-      @a1 = create :assignment
-      @a2 = create :assignment
-      @a3 = create :assignment, deleted_at: Time.zone.now
-
+      create_list :assignment, 3
       get :index, format: :json
     end
 
     it { should respond_with 200 }
-
-    it 'responds with all assignments' do
-      expect(assignments.map { |a| a['id'] }).to include @a1.id, @a2.id, @a3.id
-      expect(assignments.map { |a| a['skill_id'] }).to include @a1.skill_id, @a2.skill_id, @a3.skill_id
-      expect(assignments.map { |a| a['subject_id'] }).to include @a1.subject_id, @a2.subject_id, @a3.subject_id
-      expect(assignments.map { |a| a['deleted_at'] }).to include @a1.deleted_at, @a2.deleted_at, @a3.deleted_at.iso8601(3)
-    end
-
-    it 'responds with timestamp' do
-      expect(response_timestamp).to be_within(1.second).of Time.zone.now
-    end
-
-    it 'responds only with assignments created or updated after a certain time' do
-      create :assignment, created_at: 2.months.ago, updated_at: 2.days.ago
-      create :assignment, created_at: 2.months.ago, updated_at: 5.days.ago
-      create :assignment, created_at: 5.days.ago, updated_at: 6.hours.ago
-
-      get :index, format: :json, params: { after_timestamp: 1.day.ago }
-
-      expect(assignments.length).to eq 4
-    end
-
-    it 'excludes deleted assignments from the response' do
-      get :index, format: :json, params: { exclude_deleted: true }
-
-      expect(assignments.length).to eq 2
-      expect(assignments.map { |o| o['id'] }).to include @a1.id, @a2.id
-    end
   end
 
   describe '#show' do
@@ -58,31 +25,5 @@ RSpec.describe Api::AssignmentsController, type: :controller do
     end
 
     it { should respond_with 200 }
-
-    it 'responds with a specific assignment' do
-      expect(assignment['id']).to eq @a.id
-      expect(assignment['subject_id']).to eq @a.subject_id
-      expect(assignment['skill_id']).to eq @a.skill_id
-    end
-
-    it 'responds with timestamp' do
-      expect(response_timestamp).to be_within(1.second).of Time.zone.now
-    end
-
-    describe 'include' do
-      it 'includes the skill' do
-        get :show, format: :json, params: { id: @a.id, include: 'skill' }
-
-        expect(assignment['skill']['id']).to eq @a.skill.id
-        expect(assignment['skill']['skill_name']).to eq @a.skill.skill_name
-      end
-
-      it 'includes the subject' do
-        get :show, format: :json, params: { id: @a.id, include: 'subject' }
-
-        expect(assignment['subject']['id']).to eq @a.subject.id
-        expect(assignment['subject']['subject_name']).to eq @a.subject.subject_name
-      end
-    end
   end
 end
