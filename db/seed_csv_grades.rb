8e7ed691c78791d3401345cb9b907ffc6dd12954
Tomislav Pesut <tomca32@gmail.com>
2017-04-19 20:57:19 -0400

    TRACK-110 - Seeding DOB from csv data

--- db/seed_csv_grades.rb
+++ db/seed_csv_grades.rb
@@ -3,38 +3,31 @@
 require 'smarter_csv'
 
 class CSVDataSeeder
+  DEFAULT_SKILL_NAMES = {
+    memorization: 'Memorization',
+    grit: 'Grit',
+    teamwork: 'Teamwork',
+    discipline: 'Discipline',
+    self_esteem: 'Self-Esteem',
+    creativity: 'Creativity & Self-Expression',
+    language: 'Language'
+  }.freeze
+
   def initialize(csv_file_path = './db/seed_data/realistic_data.csv')
     @csv_file_path = csv_file_path
     @groups = {}
     @students = {}
     @lessons = {}
     @group_names = { :'1' => 'A', :'2' => 'B', :'3' => 'C' }
-    @skill_names = {
-      memorization: 'Memorization',
-      grit: 'Grit',
-      teamwork: 'Teamwork',
-      discipline: 'Discipline',
-      self_esteem: 'Self-Esteem',
-      creativity: 'Creativity & Self-Expression',
-      language: 'Language'
-    }
   end
 
   def seed_data(chapter, default_subject)
     @subject = default_subject
     read_csv_file.each do |r|
       create_group(r[:group], chapter)
-      create_student(r[:id], r[:child_name], r[:last_name], r[:group], chapter, r[:gender] - 1)
+      create_student(r[:id], r[:child_name], r[:last_name], r[:group], chapter, r[:gender] - 1, r[:age])
       create_lesson(r[:group], r[:date])
-      grade_student @students[:"#{r[:id]}"], @lessons[:"#{r[:group]}-#{r[:date]}"], {
-        memorization: r[:memorization],
-        grit: r[:grit],
-        teamwork: r[:teamwork],
-        discipline: r[:discipline],
-        self_esteem: r[:self_esteem],
-        creativity: r[:creativity],
-        language: r[:language]
-      }
+      grade_student @students[:"#{r[:id]}"], @lessons[:"#{r[:group]}-#{r[:date]}"], get_row_grades(r)
     end
   end
 
@@ -50,10 +43,10 @@ class CSVDataSeeder
     @groups[:"#{group_id}"] = chapter.groups.create(group_name: @group_names[:"#{group_id}"])
   end
 
-  def create_student(id, first_name, last_name, group_id, chapter, gender)
+  def create_student(id, first_name, last_name, group_id, chapter, gender, age)
     return if @students.key? :"#{id}"
 
-    @students[:"#{id}"] = Student.create(mlid: id, first_name: first_name, last_name: last_name, gender: gender, dob: '2003-01-01', estimated_dob: 'true', group: @groups[:"#{group_id}"], organization: chapter.organization)
+    @students[:"#{id}"] = Student.create(mlid: id, first_name: first_name, last_name: last_name, gender: gender, dob: age.years.ago, estimated_dob: 'true', group: @groups[:"#{group_id}"], organization: chapter.organization)
   end
 
   def create_lesson(group_id, date)
@@ -62,10 +55,22 @@ class CSVDataSeeder
     @lessons[:"#{group_id}-#{date}"] = Lesson.create group: @groups[:"#{group_id}"], date: d, subject: @subject
   end
 
+  def get_row_grades(row)
+    {
+      memorization: row[:memorization],
+      grit: row[:grit],
+      teamwork: row[:teamwork],
+      discipline: row[:discipline],
+      self_esteem: row[:self_esteem],
+      creativity: row[:creativity],
+      language: row[:language]
+    }
+  end
+
   def grade_student(student, lesson, grades)
     grades.each do |skill_name, mark|
       return lesson.mark_student_as_absent(student) if mark.nil?
-      skill = Skill.where(skill_name: @skill_names[skill_name]).first
+      skill = Skill.where(skill_name: DEFAULT_SKILL_NAMES[skill_name]).first
       gd = GradeDescriptor.where(skill: skill, mark: mark).first
       Grade.create lesson: lesson, student: student, grade_descriptor: gd
     end
