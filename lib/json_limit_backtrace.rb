28df39ee92c9aac7b4432dc984c245b5dddac85b
Tomislav Pesut <tomca32@gmail.com>
2018-04-09 15:40:20 -0400

    Add exception message to log message field

--- lib/json_limit_backtrace.rb
+++ lib/json_limit_backtrace.rb
@@ -6,5 +6,6 @@ class JsonLimitBacktrace < SemanticLogger::Formatters::Json
     super
     # limits the stack trace length to 10 lines
     hash[:exception][:stack_trace] = hash[:exception][:stack_trace].take 10
+    hash[:message] = hash[:exception][:message]
   end
 end
