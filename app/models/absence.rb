19d1f62b0ec5e17cfc8b49a8a7bfea239fc0fa61
Tomislav Pesut <tomca32@gmail.com>
2019-03-20 20:43:46 -0400

    Removed old absence rules

--- app/models/absence.rb
+++ app/models/absence.rb
@@ -1,17 +1,10 @@
 # frozen_string_literal: true
 
 class Absence < ApplicationRecord
-  before_validation :update_uid
   belongs_to :student
   belongs_to :lesson
-
-  validates :lesson_uid, presence: true
 
   validates :student, uniqueness: {
     scope: :lesson_id
   }
-
-  def update_uid
-    self.lesson_uid = lesson&.reload&.uid
-  end
 end
