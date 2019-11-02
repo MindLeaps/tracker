80347cf085b9c620bc09144830af39dd145eae01
Tomislav Pesut <tomca32@gmail.com>
2019-11-02 13:31:03 -0400

    #712 - Added the #around calculation for group lesson summaries

--- app/models/group_lesson_summary.rb
+++ app/models/group_lesson_summary.rb
@@ -1,10 +1,28 @@
 # frozen_string_literal: true
 
 class GroupLessonSummary < ApplicationRecord
-  self.primary_key = :lesson_id
+  self.primary_key = :lesson_uid
   belongs_to :chapter
 
   def readonly?
     true
+  end
+
+  def around(number_of_elements)
+    before = GroupLessonSummary
+               .where('lesson_date <= ? AND group_id = ?', lesson_date, group_id)
+               .order(lesson_date: :desc).limit(number_of_elements)
+               .pluck(Arel.sql('extract(EPOCH FROM lesson_date) :: INT'), :average_mark, Arel.sql("to_char(lesson_date, 'MM/DD/YY')"), :lesson_uid, :lesson_id)
+    after = GroupLessonSummary
+              .where('lesson_date > ? AND group_id = ?', lesson_date, group_id)
+              .order(:lesson_date).limit(number_of_elements)
+              .pluck(Arel.sql('extract(EPOCH FROM lesson_date) :: INT'), :average_mark, Arel.sql("to_char(lesson_date, 'MM/DD/YY')"), :lesson_uid, :lesson_id)
+    result = before.reverse + after
+    index = result.index { |l| l[3] == lesson_uid }
+    number_of_elements = [result.size, number_of_elements].min
+    result[
+      [[index - (number_of_elements / 2), 0].max, result.size - number_of_elements].min,
+      number_of_elements
+    ]
   end
 end
---
+++ spec/models/group_lesson_summary_spec.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe GroupLessonSummary, type: :model do
+
+  describe '#readonly?' do
+    before :each do
+      @group = create :group
+      create :graded_student, group: @group, grades: { 'skill' => [1] }
+    end
+
+    it 'returns false' do
+      expect(GroupLessonSummary.first.readonly?).to eq true
+    end
+  end
+
+  describe 'query' do
+    before :each do
+      @group = create :group
+      @student = create :graded_student, group: @group, grades: {
+        'Memorization' => [1, 2, 4],
+        'Grit' => [2, 4]
+      }
+    end
+
+    it 'returns group lesson summaries with correct marks and grade count' do
+      result = GroupLessonSummary.all
+      expect(result.size).to eq 3
+
+      expect(result[0].grade_count).to eq 2
+      expect(result[0].average_mark).to eq 1.5
+
+      expect(result[1].grade_count).to eq 2
+      expect(result[1].average_mark).to eq 3
+
+      expect(result[2].grade_count).to eq 1
+      expect(result[2].average_mark).to eq 4
+    end
+  end
+
+  describe '#around' do
+    before :each do
+      @group = create :group
+      create :graded_student, group: @group, grades: {
+        'Memorization' => [1, 2, 1, 3, 2, 3, 4],
+        'Grit' => [2, 3, 5, 3, 4, 5, 6]
+      }
+    end
+    
+    it 'returns the target group lesson summary and 2 before and after it' do
+      lesson = Lesson.order(:date).all[3]
+      summary = GroupLessonSummary.find(lesson.uid)
+      result = summary.around 5
+      
+      expect(result.size).to eq 5
+      expect(result[0][1]).to eq 2.5
+      expect(result[1][1]).to eq 3
+      expect(result[2][1]).to eq 3
+      expect(result[3][1]).to eq 3
+      expect(result[4][1]).to eq 4
+    end
+
+    it 'bounds the result to the first lesson' do
+      lesson = Lesson.order(:date).all[1]
+      summary = GroupLessonSummary.find(lesson.uid)
+      result = summary.around 5
+
+      expect(result.size).to eq 5
+      expect(result[0][1]).to eq 1.5
+      expect(result[1][1]).to eq 2.5
+      expect(result[2][1]).to eq 3
+      expect(result[3][1]).to eq 3
+      expect(result[4][1]).to eq 3
+    end
+
+    it 'bounds the result to the last lesson' do
+      lesson = Lesson.order(:date).all[6]
+      summary = GroupLessonSummary.find(lesson.uid)
+      result = summary.around 5
+
+      expect(result.size).to eq 5
+      expect(result[0][1]).to eq 3
+      expect(result[1][1]).to eq 3
+      expect(result[2][1]).to eq 3
+      expect(result[3][1]).to eq 4
+      expect(result[4][1]).to eq 5
+    end
+
+    it 'correctly bounds the result when the number of elements requested is higher than the count' do
+      lesson = Lesson.order(:date).all[1]
+      summary = GroupLessonSummary.find(lesson.uid)
+      result = summary.around 8
+
+      expect(result.size).to eq 7
+      expect(result[0][1]).to eq 1.5
+      expect(result[1][1]).to eq 2.5
+      expect(result[2][1]).to eq 3
+      expect(result[3][1]).to eq 3
+      expect(result[4][1]).to eq 3
+      expect(result[5][1]).to eq 4
+      expect(result[6][1]).to eq 5
+    end
+  end
+end
