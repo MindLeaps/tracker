8244f2bc4df480f957f352b90a2dd8f77d1be5eb
Tomislav Pesut <tomca32@gmail.com>
2022-01-17 22:01:25 -0700

    Rewrote analytics filter controller to handle resource filtering logic

--- app/javascript/controllers/analytics_filter_controller.js
+++ app/javascript/controllers/analytics_filter_controller.js
@@ -1,13 +1,24 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
+
+function createOption(label, value) {
+  const opt = document.createElement('option');
+  opt.text = label;
+  if (value !== undefined) {
+    opt.value = value;
+  }
+
+  return opt;
+}
 
 export default class extends Controller {
   static targets = ['select', 'anchor']
 
   connect() {
-    this.generateUrl()
+    this.updateFilter()
   }
 
-  generateUrl() {
+  updateFilter() {
+    this.updateDropdown(this.selectTargets[0], JSON.parse(this.selectTargets[0].dataset.resources))
     this.anchorTarget.href = this.selectTargets.reduce((acc, e) => {
       return acc + e.getAttribute('data-name') + '=' + this.toId(e.value) + '&'
     }, '?')
@@ -18,5 +29,32 @@ export default class extends Controller {
       return ''
     }
     return value
+  }
+
+  updateDropdown(dropdown, values, parentIds) {
+    const currentValue = Number(dropdown.value);
+    dropdown.innerHTML = '';
+    dropdown.append(createOption('All', null));
+
+    let valueExists = false;
+    const filteredIds = values.filter(v => !parentIds || parentIds.includes(v.depend_id)).map((v) => {
+      if (currentValue === v.id) {
+        valueExists = true;
+      }
+      dropdown.append(createOption(v.label, v.id));
+      return v.id;
+    });
+
+    if (valueExists) {
+      dropdown.value = currentValue;
+    }
+
+    const dependents = JSON.parse(dropdown.dataset.dependents || null);
+    if (dependents) {
+      dependents.forEach(dependentSelectName => {
+        const targetSelect = this.selectTargets.find(t => t.dataset.name === dependentSelectName);
+        this.updateDropdown(targetSelect, JSON.parse(targetSelect.dataset.resources), valueExists ? [currentValue] : filteredIds)
+      })
+    }
   }
 }
