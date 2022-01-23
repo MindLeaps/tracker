a913e077fbae3b409c92c03d63fbee06297e6910
Tomislav Pesut <tomca32@gmail.com>
2022-01-22 20:51:06 -0700

    Revert back to headless chrome with a larger window size

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -99,7 +99,8 @@ RSpec.configure do |config|
     version = Capybara::Selenium::Driver.load_selenium
     options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
     browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
-      opts.add_argument('--disable-gpu')
+      opts.add_argument('--headless') # Remove this to see tests running in a browser
+      opts.add_argument('--window-size=2500,2500')
       # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
       opts.add_argument('--disable-site-isolation-trials')
       opts.add_preference('download.default_directory', Capybara.save_path)
@@ -109,7 +110,6 @@ RSpec.configure do |config|
     Capybara::Selenium::Driver.new(app, **{ browser: :chrome, options_key => browser_options })
   end
 
-  # Change this to :selenium_chrome to see the tests running in the browser
   Capybara.javascript_driver = :chrome
   Capybara.server = :puma, { Silent: true }
 end
