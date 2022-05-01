f402811dfdd273e03a7b4a9bfe19184efed6a017
Tomislav Pesut <tomca32@gmail.com>
2022-04-30 21:43:42 -0700

    Added Users table

---
+++ app/components/table_components/user_row.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class TableComponents::UserRow < TableComponents::BaseRow
+  def self.columns
+    [
+      { column_name: '#' },
+      { order_key: :name, column_name: I18n.t(:name) },
+      { order_key: :email, column_name: I18n.t(:email) },
+      { column_name: I18n.t(:global_role) }
+    ]
+  end
+
+  def global_role
+    @item.global_role? ? @item.global_role.label : I18n.t(:none)
+  end
+end
