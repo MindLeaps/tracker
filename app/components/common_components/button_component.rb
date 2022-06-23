a6813ea1428e43219e44bf1d14b372f0fa02624c
Tomislav Pesut <tomca32@gmail.com>
2022-06-22 17:26:12 -0700

    Redesigned General analytics

--- app/components/common_components/button_component.rb
+++ app/components/common_components/button_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class CommonComponents::ButtonComponent < ViewComponent::Base
-  def initialize(label:, href:)
+  def initialize(label:, href: nil, attributes: '')
     @label = label
     @href = href
+    @attributes = attributes
   end
 end
--- app/components/layout_components/header_component.rb
+++ app/components/layout_components/header_component.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class LayoutComponents::HeaderComponent < LayoutComponents::SidebarComponent
+  renders_one :left
   def initialize(current_user:, tabs:, buttons: [])
     @current_user = current_user
     @tabs = tabs
