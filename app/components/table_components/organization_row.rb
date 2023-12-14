24031f697a341f67ff8e1356aca16afa4e7be0e9
Tomislav Pesut <tomca32@gmail.com>
2023-12-13 20:09:48 -0700

    fix calling orgs chapters in org table

--- app/components/table_components/organization_row.rb
+++ app/components/table_components/organization_row.rb
@@ -5,7 +5,7 @@ class TableComponents::OrganizationRow < TableComponents::BaseRow
     [
       { column_name: '#', numeric: true },
       { order_key: :mlid, column_name: I18n.t(:mlid), numeric: true },
-      { order_key: :organization_name, column_name: I18n.t(:chapter) },
+      { order_key: :organization_name, column_name: I18n.t(:organization) },
       { order_key: :chapter_count, column_name: I18n.t(:number_of_chapters), numeric: true },
       { order_key: :group_count, column_name: I18n.t(:number_of_groups), numeric: true },
       { order_key: :student_count, column_name: I18n.t(:number_of_students), numeric: true },
