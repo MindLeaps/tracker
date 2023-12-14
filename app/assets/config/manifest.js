a8c8672c3a9e775f70197d7550cc104bfd675009
Tomislav Pesut <tomca32@gmail.com>
2023-12-13 20:04:04 -0700

    remove application.css from asset pipeline

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,11 +1,9 @@
 // app/assets/config/manifest.js
 //
 //= link tailwind.css
-//= link application.css
 //= link application.js
 //= link charts.js
 //= link analytics.js
-
 //= link_tree ../images
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
