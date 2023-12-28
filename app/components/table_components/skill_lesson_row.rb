b22d285fbca0ac27a5ae2c6e12c7e463bb8a2498
Tomislav Pesut <tomca32@gmail.com>
2023-12-28 12:02:49 -0700

    fix skill table in individual lessons

--- app/components/table_components/skill_lesson_row.rb
+++ app/components/table_components/skill_lesson_row.rb
@@ -5,7 +5,7 @@ class TableComponents::SkillLessonRow < TableComponents::BaseRow
       <div class="<%= 'shaded-row' if shaded? %> table-row-wrapper">
       <a href="<%= helpers.skill_path @item.skill_id %>" class="<%= 'shaded-row' if shaded? %> table-row-wrapper">
       <div class="relative px-6 py-3 whitespace-nowrap text-sm font-medium text-gray-600 "><%= @pagy.from + @item_counter %></div>
-        <div class="table-cell "><%= @item.skill_name %></div>
+        <div class="table-cell text-right"><%= @item.skill_name %></div>
         <div class="table-cell "><%= @item.average_mark %></div>
         <div class="table-cell "><%= @item.grade_count %></div>
       </a>
@@ -16,8 +16,8 @@ class TableComponents::SkillLessonRow < TableComponents::BaseRow
     [
       { column_name: '#', numeric: true },
       { order_key: :skill, column_name: I18n.t(:skill) },
-      { order_key: :average, column_name: I18n.t(:average) },
-      { order_key: :grades, column_name: I18n.t(:grades) }
+      { order_key: :average, column_name: I18n.t(:average), numeric: true },
+      { order_key: :grades, column_name: I18n.t(:grades), numeric: true }
     ]
   end
 end
