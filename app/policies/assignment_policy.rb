f57f6ddde3a6c2b4a04e5fb922ae371104d918d7
KralMarko123 <kralmarko123@gmail.com>
2024-03-25 00:24:07 +0100

    fix: trailing whitespace

--- app/policies/assignment_policy.rb
+++ app/policies/assignment_policy.rb
@@ -7,8 +7,7 @@ class AssignmentPolicy < ApplicationPolicy
     def resolve
       return scope.all if user.global_role?
 
-      scope.joins('LEFT JOIN skills on skill_id = skills.id 
-                   LEFT JOIN subjects on subject_id = subjects.id')
+      scope.joins('LEFT JOIN skills on skill_id = skills.id LEFT JOIN subjects on subject_id = subjects.id')
            .where('skills.organization_id IN (:org_ids) OR subjects.organization_id IN (:org_ids)', org_ids: user.roles.select(:resource_id))
     end
   end
