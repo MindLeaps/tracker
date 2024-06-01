8ba6c12c616792e3c4bbfe4f082d830021a43a22
KralMarko123 <kralmarko123@gmail.com>
2024-06-01 18:23:29 +0200

    feat: updated migration

--- db/migrate/20240517085142_change_lesson_id_column_type_and_copy_values.rb
+++ db/migrate/20240517085142_change_lesson_id_column_type_and_copy_values.rb
@@ -3,23 +3,19 @@ class ChangeLessonIdColumnTypeAndCopyValues < ActiveRecord::Migration[7.0]
     # rubocop:disable Rails/SkipsModelValidations
     # rubocop:disable Rails/DangerousColumnNames
     rename_column(:lessons, :id, :old_id)
-    rename_column(:grades, :lesson_id, :old_lesson_id)
-    add_column(:absences, :lesson_uid, :uuid)
-
     rename_column(:lessons, :uid, :id)
-    execute 'ALTER TABLE lessons ADD PRIMARY KEY (id);'
+    rename_column(:grades, :lesson_id, :old_lesson_id)
     rename_column(:grades, :lesson_uid, :lesson_id)
     rename_column(:absences, :lesson_id, :old_lesson_id)
-    rename_column(:absences, :lesson_uid, :lesson_id)
+    add_column(:absences, :lesson_id, :uuid)
 
-    Absence.update_all(lesson: Lesson.find(id: :old_lesson_id))
+    Absence.update_all(lesson_id: Lesson.find_by(old_id: :old_lesson_id).id)
     change_column(:absences, :lesson_id, :uuid, null: false)
     # rubocop:enable Rails/DangerousColumnNames
     # rubocop:enable Rails/SkipsModelValidations
   end
 
   def down
-    # rubocop:disable Rails/SkipsModelValidations
     # rubocop:disable Rails/DangerousColumnNames
     rename_column(:grades, :lesson_id, :lesson_uid)
     rename_column(:grades, :old_lesson_id, :lesson_id)
@@ -30,6 +26,5 @@ class ChangeLessonIdColumnTypeAndCopyValues < ActiveRecord::Migration[7.0]
 
     remove_column(:absences, :lesson_uid)
     # rubocop:enable Rails/DangerousColumnNames
-    # rubocop:enable Rails/SkipsModelValidations
   end
 end
