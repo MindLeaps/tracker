fb8444614c17c4d2e819e69255e84f8d95032682
KralMarko123 <kralmarko123@gmail.com>
2024-06-12 12:14:18 +0200

    chore: recreate absences table during rollback

--- db/migrate/20240610074002_drop_absences.rb
+++ db/migrate/20240610074002_drop_absences.rb
@@ -2,10 +2,20 @@ class DropAbsences < ActiveRecord::Migration[7.1]
   def up
     drop_view :student_lesson_details
     drop_table :absences
+
     create_view :student_lesson_details, version: 3
   end
 
   def down
-    raise ActiveRecord::IrreversibleMigration
+    drop_view :student_lesson_details
+
+    # rubocop:disable Rails/CreateTableWithTimestamps
+    create_table :absences do |t|
+      t.references :student, foreign_key: true, null: false
+      t.references :lesson, type: :uuid, foreign_key: true, null: false
+    end
+    # rubocop:enable Rails/CreateTableWithTimestamps
+
+    create_view :student_lesson_details, version: 2
   end
 end
