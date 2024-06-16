4af3f9b744e6e333f3dedc1523e123236ce80518
Nenad-Stoj <104387319+Nenad-Stoj@users.noreply.github.com>
2024-06-16 17:04:44 +0200

    #1903:Analytics on student page

--- app/assets/javascripts/charts.js
+++ app/assets/javascripts/charts.js
@@ -50,3 +50,52 @@ function displayLessonGraph(containerId, lessonUid, data) {
   });
 
 }
+
+function displayStudentLessons(containerId, studentId, data) {
+  new Chartist.Line(containerId, {
+    series: [
+      {
+        name: 'Grades',
+        data: data.map(function(e) {
+          return {
+            x: new Date(e.lesson_date).getTime(), // Convert to timestamp in milliseconds
+            y: e.average_mark,
+            meta: {
+              student_id: studentId
+            }
+          };
+        })
+      }
+    ]
+  }, {
+    axisX: {
+      type: Chartist.FixedScaleAxis,
+      divisor: 5,
+      labelInterpolationFnc: function (value) {
+        return new Date(value).toLocaleDateString('en-US'); // Convert timestamp back to date
+      }
+    },
+    axisY: {
+      onlyInteger: true,
+      low: 1,
+      high: 7
+    },
+    plugins: [
+      function(chart) {
+        chart.on('draw', function (data) {
+          if (data.type === 'point') {
+            if (data.meta.student_id === studentId) {
+              data.element.attr({
+                style: 'stroke: #4CAF50; stroke-width: 10px; fill: #4CAF50;'
+              });
+            }
+            data.element._node.setAttribute('style', 'cursor: pointer');
+          }
+        });
+      }
+    ]
+  });
+}
+
+
+
