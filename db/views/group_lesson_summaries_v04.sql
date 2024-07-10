2f4c0b1b54bcddc4f9337d1d07b6e3755528cc29
KralMarko123 <kralmarko123@gmail.com>
2024-07-10 16:00:49 +0200

    feat: modified view query due to performance

--- db/views/group_lesson_summaries_v04.sql
+++ db/views/group_lesson_summaries_v04.sql
@@ -1,15 +1,14 @@
-SELECT	  l.id AS lesson_id,
-            l.date AS lesson_date,
-            gr.id AS group_id,
-            gr.chapter_id AS chapter_id,
-            slu.subject_id,
-            CONCAT(gr.group_name, ' - ', c.chapter_name) AS group_chapter_name,
-            ROUND(CAST(AVG(average_mark) AS numeric), 2)::FLOAT AS average_mark,
-            CAST(SUM(grade_count) AS bigint) AS grade_count
-FROM lessons AS l
-         JOIN groups gr ON l.group_id = gr.id
+SELECT	        slu.lesson_id AS lesson_id,
+                slu.lesson_date AS lesson_date,
+                gr.id AS group_id,
+                gr.chapter_id AS chapter_id,
+                slu.subject_id,
+                CONCAT(gr.group_name, ' - ', c.chapter_name) AS group_chapter_name,
+                ROUND(CAST(AVG(average_mark) AS numeric), 2)::FLOAT AS average_mark,
+                CAST(SUM(grade_count) AS bigint) AS grade_count
+FROM student_lesson_summaries AS slu
+         JOIN groups gr ON slu.group_id = gr.id
          JOIN chapters c ON gr.chapter_id = c.id
-         JOIN student_lesson_summaries slu ON l.subject_id = slu.subject_id AND l.group_id = slu.group_id AND l.date = slu.lesson_date
 WHERE slu.deleted_at IS NULL
-GROUP BY l.id, gr.id, c.id, slu.subject_id
+GROUP BY slu.lesson_id, gr.id, c.id, slu.subject_id, slu.lesson_date
 ORDER BY lesson_date
