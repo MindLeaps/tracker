e01429fd3b0ac6fae7b6add877e52c46fd21e328
Nenad-Stoj <104387319+Nenad-Stoj@users.noreply.github.com>
2024-08-01 16:04:05 +0200

    init POC

---
+++ app/components/table_components/student_row_report.rb
@@ -0,0 +1,21 @@
+class TableComponents::StudentRowReport < TableComponents::BaseRow
+  erb_template <<~ERB
+    <div class="<%= 'shaded-row' if shaded? %> <%= 'deleted-row' if @item.deleted_at? %> table-row-wrapper student-row">
+        <div class="text-right table-cell "><%= @item.last_name %></div>
+        <div class="text-right table-cell "><%= @item.first_name %></div>
+        <div class="text-right table-cell "><%= @item.first_lesson %></div>
+        <div class="text-right table-cell "><%= @item.middle_lesson %></div>
+        <div class="text-right table-cell "><%= @item.last_lesson %></div>
+    </div>
+  ERB
+
+  def self.columns
+    [
+      { order_key: :last_name, column_name: I18n.t(:last_name) },
+      { order_key: :first_name, column_name: I18n.t(:first_name) },
+      { order_key: :first_lesson, column_name: I18n.t(:first_lesson) },
+      { order_key: :middle_lesson, column_name: I18n.t(:middle_lesson) },
+      { order_key: :last_lesson, column_name: I18n.t(:last_lesson) }
+    ]
+  end
+end
--- app/controllers/group_reports_controller.rb
+++ app/controllers/group_reports_controller.rb
@@ -4,6 +4,27 @@ class GroupReportsController < HtmlController
   def show
     @group = Group.includes(:chapter).find params[:id]
     authorize @group
+    group_summaries = GroupLessonSummary.where(group_id: @group.id).where.not(average_mark: nil).order(lesson_date: :asc)
+    @first_lesson = { lesson_date: group_summaries.first.lesson_date, average_mark: group_summaries.first.average_mark }
+    @last_lesson = { lesson_date: group_summaries.last.lesson_date, average_mark: group_summaries.last.average_mark }
+    @middle_lesson = { lesson_date: group_summaries.offset(group_summaries.size / 2).first.lesson_date, average_mark: group_summaries.offset(group_summaries.size / 2).first.average_mark }
 
+    @student_rows = apply_scopes(StudentLessonSummary.where(group_id: @group.id))
+    @student_table_component = TableComponents::Table.new(pagy: nil, rows: @student_rows, row_component: TableComponents::StudentRowReport)
+
+    @lesson_summaries = GroupLessonSummary.where(group_id: @group.id).where.not(average_mark: nil).order(lesson_date: :asc).last(30).map do |summary|
+      {
+        lesson_date: summary.lesson_date,
+        average_mark: summary.average_mark
+      }
+    end
+
+    @lesson_summaries_lifetime = GroupLessonSummary.where(group_id: @group.id).where.not(average_mark: nil).order(lesson_date: :asc).map do |summary|
+      {
+        lesson_date: summary.lesson_date,
+        average_mark: summary.average_mark
+      }
+    end
+    @student_table_component = TableComponents::Table.new(pagy: @pagy, rows: @student_rows, row_component: TableComponents::StudentRowReport)
   end
 end
