1c44690b1673f83feb1b4aa4603828f45e4a31c3
KralMarko123 <kralmarko123@gmail.com>
2024-09-02 01:13:08 +0300

    chore: raise irreversible migration

--- db/migrate/20240723120532_remove_old_lesson_id.rb
+++ db/migrate/20240723120532_remove_old_lesson_id.rb
@@ -1,10 +1,12 @@
 class RemoveOldLessonId < ActiveRecord::Migration[7.1]
-  def change
-    # rubocop:disable Rails/ReversibleMigration
+  def up
     drop_view :lesson_table_rows
     remove_column :grades, :lesson_old_id
     remove_column :lessons, :old_id
     create_view :lesson_table_rows, version: 3
-    # rubocop:enable Rails/ReversibleMigration
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration, 'This migration cannot be reverted because it destroys data.'
   end
 end
