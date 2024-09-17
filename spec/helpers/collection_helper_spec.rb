f473809e9df2bf704aebc6b783b0b9dec0b04984
KralMarko123 <kralmarko123@gmail.com>
2024-09-17 15:32:26 +0200

    chore: rubocop fix

--- spec/helpers/collection_helper_spec.rb
+++ spec/helpers/collection_helper_spec.rb
@@ -1,7 +1,7 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe CollectionHelper, type: :helper do
-  describe "helps with collections" do
+  describe 'helps with collections' do
     before :each do
       @even_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
       @uneven_array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -11,12 +11,12 @@ RSpec.describe CollectionHelper, type: :helper do
       create :group, group_name: 'C'
     end
 
-    it "returns the middle element from an array" do
+    it 'returns the middle element from an array' do
       expect(middle_from_array(@even_array)).to eq 5
       expect(middle_from_array(@uneven_array)).to eq 5
     end
 
-    it "returns the middle element from a relation" do
+    it 'returns the middle element from a relation' do
       ordered_groups = Group.order(:group_name)
 
       expect(middle_from_rel(ordered_groups).group_name).to eq 'B'
