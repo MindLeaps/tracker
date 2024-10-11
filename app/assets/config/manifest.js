18a0c82d228f1b96805c748b3864e18bec013514
KralMarko123 <kralmarko123@gmail.com>
2024-10-11 20:16:26 +0200

    chore: remove space

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -6,6 +6,6 @@
 //= link analytics.js
 //= link frappe-gantt.css
 //= link_tree ../images
-// = link_tree ../../javascript .js
+//= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 
