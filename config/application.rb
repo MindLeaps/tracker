ccc855eb6aac7bdfb1c79ffa87a58e84b59c5911
Tomislav Pesut <tomca32@gmail.com>
2024-10-27 19:25:57 -0700

    #2022 - Make sure to export schema in structure.sql file

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Tracker
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
+    config.active_record.schema_format = :sql
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
