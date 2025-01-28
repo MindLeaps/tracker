268f9e0a29ce35c3f969f3b65b23cf995eed66e7
KralMarko123 <kralmarko123@gmail.com>
2025-01-29 00:07:45 +0100

    feat: defaulted toggles to show, added simple tests

--- app/components/common_components/toggle_component.rb
+++ app/components/common_components/toggle_component.rb
@@ -1,7 +1,8 @@
 class CommonComponents::ToggleComponent < ViewComponent::Base
   include ApplicationHelper
-  def initialize(id:, text:)
+  def initialize(id:, text:, is_toggled: true)
     @id = id
     @text = text
+    @is_toggled = is_toggled
   end
 end
