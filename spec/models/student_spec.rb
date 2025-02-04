27bdc8785194d2adb31222952331d1dea1a0af32
KralMarko123 <kralmarko123@gmail.com>
2025-02-04 10:23:37 +0100

    test: small fix for a test that sometimes fails in CI

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -189,6 +189,7 @@ RSpec.describe Student, type: :model do
     describe 'search' do
       before :each do
         @zombarato = create :student, first_name: 'Zombarato', last_name: 'Agustato', mlid: 'Z31'
+        @zombanavo = create :student, first_name: 'Zombanavo', last_name: 'Domboklat', mlid: 'Z32'
         @zombaruto = create :student, first_name: 'Zombaruto', last_name: 'Agurat'
         @zomzovato = create :student, first_name: 'Zomzovato', last_name: 'Domovat'
       end
@@ -206,21 +207,27 @@ RSpec.describe Student, type: :model do
       end
 
       it 'finds the student by the MLID match' do
-        result = Student.search('Z3')
+        result = Student.search('Z31')
         expect(result.length).to eq 1
         expect(result).to include @zombarato
       end
 
+      it 'finds students by a partial MLID match' do
+        result = Student.search('Z3')
+        expect(result.length).to eq 2
+        expect(result).to include @zombarato, @zombanavo
+      end
+
       it 'finds students by a partial first name match' do
         result = Student.search('Zomb')
-        expect(result.length).to eq 2
-        expect(result).to include @zombarato, @zombaruto
+        expect(result.length).to eq 3
+        expect(result).to include @zombarato, @zombaruto, @zombanavo
       end
 
       it 'finds students by a partial last name match' do
         result = Student.search('Dom')
-        expect(result.length).to eq 1
-        expect(result).to include @zomzovato
+        expect(result.length).to eq 2
+        expect(result).to include @zomzovato, @zombanavo
       end
     end
   end
