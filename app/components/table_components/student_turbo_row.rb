7946eabfab5fceb1cb1545cedd70a0979f042bf8
KralMarko123 <kralmarko123@gmail.com>
2025-06-04 17:25:48 +0200

    feat: pass group from controller in views for student rows to use

--- app/components/table_components/student_turbo_row.rb
+++ app/components/table_components/student_turbo_row.rb
@@ -14,4 +14,9 @@ class TableComponents::StudentTurboRow < TableComponents::BaseRow
   def self.row_style
     "grid-template-columns: repeat(#{TableComponents::StudentTurboRow.columns.count}, auto);"
   end
+
+  def initialize(item:, item_counter:, pagy:, group:)
+    super(item: item, item_counter: item_counter, pagy: pagy)
+    @group = group
+  end
 end
--- app/controllers/group_students_controller.rb
+++ app/controllers/group_students_controller.rb
@@ -16,30 +16,27 @@ class GroupStudentsController < HtmlController
     authorize @student
   end
 
-  # rubocop:disable Metrics/AbcSize
   def create
-    @group = Group.includes(:chapter).find(params.require(:group_id))
+    @group = Group.find_by(id: params.require(:group_id))
+    authorize @group
+
     @student = Student.new(inline_student_params)
     @student.organization_id = @group.chapter.organization.id
-    @student.current_group_id = @group.id
     authorize @student
-    @new_student_enrollment = Enrollment.new(group: @group, student: @student, active_since: Time.zone.now)
 
-    if @student.save && @new_student_enrollment.save
+    if @student.save
       success_now title: t(:student_added), text: t(:student_name_added, name: @student.proper_name)
     else
       failure_now title: t(:student_invalid), text: t(:fix_form_errors)
       render :new, status: :unprocessable_entity
     end
   end
-  # rubocop:enable Metrics/AbcSize
 
   def update
     @student = Student.find_by(id: params.require(:id))
     @group = Group.find_by(id: params.require(:group_id))
     authorize @student
     @pagy, @students = pagy(group_students)
-    @student.current_group_id = @group.id
 
     if @student.update(inline_student_params)
       success_now title: t(:student_updated), text: t(:student_name_updated, name: @student.proper_name)
@@ -53,12 +50,8 @@ class GroupStudentsController < HtmlController
     @group = Group.find_by(id: params.require(:group_id))
     authorize @group, :show?
     @student = Student.find_by(id: params.require(:id))
-    @student.current_group_id = @group.id
 
     @pagy, @students = pagy(@group.students)
-    @students.each do |s|
-      s.current_group_id = @group.id
-    end
   end
 
   def destroy
