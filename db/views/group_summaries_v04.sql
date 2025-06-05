74acadd9b3ca2c7e65b7f1d06defaad7912ba17e
Marko Markovikj <m.markovikj@nextech.com>
2025-06-05 16:17:57 +0200

    feat: fix group_summaries view student count

--- db/views/group_summaries_v04.sql
+++ db/views/group_summaries_v04.sql
@@ -10,9 +10,10 @@ SELECT g.id,
        g.mlid         AS mlid,
        CONCAT(o.mlid, '-', c.mlid, '-', g.mlid) AS full_mlid,
        organization_name,
-       SUM(CASE WHEN (en.active_since <= CURRENT_DATE) AND (en.inactive_since IS NULL OR en.inactive_since >= CURRENT_DATE) THEN 1 ELSE 0 END) AS student_count
+       SUM(CASE WHEN (en.active_since <= CURRENT_DATE) AND (en.inactive_since IS NULL OR en.inactive_since >= CURRENT_DATE) AND (s.deleted_at IS NULL) THEN 1 ELSE 0 END) AS student_count
 FROM groups g
          LEFT JOIN enrollments en ON g.id = en.group_id
+         LEFT JOIN students s ON s.id = en.student_id
          LEFT JOIN chapters c ON g.chapter_id = c.id
          LEFT JOIN organizations o ON c.organization_id = o.id
 GROUP BY g.id, c.id, o.id;
