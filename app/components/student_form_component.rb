3f40ffd4b806bf037fdf6e3f743ed6400e37d384
KralMarko123 <kralmarko123@gmail.com>
2025-06-12 16:34:37 +0200

    feat: used dynamic fields controller to update organization and enrollment dropdowns dynamically

--- app/components/student_form_component.rb
+++ app/components/student_form_component.rb
@@ -3,9 +3,10 @@ class StudentFormComponent < ViewComponent::Base
   attr_reader :chapter_groups, :permitted_tags
 
   class ChapterGroups
-    attr_reader :groups, :chapter_display, :organization_id
+    attr_reader :id, :groups, :chapter_display, :organization_id
 
     def initialize(chapter, permitted_groups)
+      @id = chapter.id
       @groups = permitted_groups.filter { |g| g.chapter_id == chapter.id }.sort_by(&:chapter_group_name_with_full_mlid)
       @chapter_display = "#{chapter.organization_name} - #{chapter.chapter_name} (#{chapter.full_mlid})"
       @organization_id = chapter.organization_id
--- app/javascript/controllers/dynamic_fields_controller.js
+++ app/javascript/controllers/dynamic_fields_controller.js
@@ -2,6 +2,81 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dynamic-fields"
 export default class extends Controller {
+  static targets = ['select']
+
   connect() {
+    this.updateFilter()
   }
+
+  updateFilter() {
+    this.updateDropdown(this.selectTargets[0], JSON.parse(this.selectTargets[0].dataset.resources))
+  }
+
+  updateDropdown(dropdown, values, parentIds) {
+    const currentValue = Number(dropdown.value);
+    dropdown.innerHTML = '';
+
+    let valueExists = false;
+    const filteredIds = values.filter(v => !parentIds || parentIds.includes(v.depend_id)).map((v) => {
+      if (currentValue === v.id) {
+        valueExists = true;
+      }
+      else if (v.grouped_options) {
+        v.grouped_options.forEach(o => {
+          if(currentValue === o.id) {
+            valueExists = true
+          }
+        })
+      }
+
+      if(v.grouped_options)
+        dropdown.append(createOptionGroup(v.label, v.grouped_options));
+      else
+        dropdown.append(createOption(v.label, v.id));
+
+      return v.id;
+    });
+
+    if (valueExists) {
+      dropdown.value = currentValue;
+    }
+
+    const dependents = JSON.parse(dropdown.dataset.dependents || null);
+
+    if (dependents) {
+      dependents.forEach(dependentSelectName => {
+        const targetSelects = this.selectTargets.filter(t => t.dataset.name === dependentSelectName);
+
+        targetSelects.forEach(ts => {
+          this.updateDropdown(ts, JSON.parse(ts.dataset.resources), valueExists ? [currentValue] : filteredIds)
+        })
+      })
+    }
+  }
+}
+
+function createOption(label, value) {
+  const opt = document.createElement('option');
+
+  opt.text = label;
+
+  if (value !== undefined) {
+    opt.value = value;
+  }
+
+  return opt;
+}
+
+function createOptionGroup(label, options) {
+  const optgroup = document.createElement('optgroup');
+
+  optgroup.label = label;
+
+  if (options !== undefined) {
+    options.forEach( o => {
+      optgroup.append(createOption(o.label, o.id))
+    })
+  }
+
+  return optgroup;
 }
