e9bfbaca9722d2eac0a404454a283c479c3bbb00
KralMarko123 <kralmarko123@gmail.com>
2025-10-10 00:32:31 +0200

    spec: added specs testing the new view, styling

--- app/javascript/controllers/analytics_filter_controller.js
+++ app/javascript/controllers/analytics_filter_controller.js
@@ -37,8 +37,7 @@ export default class extends Controller {
     dropdown.append(createOption('All', null));
 
     let valueExists = false;
-    const filteredIds = values.filter(v => !parentIds || parentIds.includes(v.depend_id) ||
-        ( v.dependent_ids && v.dependent_ids.some(id => parentIds.includes(id))))
+    const filteredIds = values.filter(v => !parentIds || parentIds.includes(v.depend_id) || (v.dependent_ids && v.dependent_ids.some(id => parentIds.includes(id))))
       .map((v) => {
         if (currentValue === v.id) {
           valueExists = true;
