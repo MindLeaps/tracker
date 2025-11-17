3aada35e4cc69ec03d043fe9db2f0df0bef14b44
KralMarko123 <kralmarko123@gmail.com>
2025-11-17 11:54:11 +0100

    feat: modify layout, extend unordered columns to accept column span argument

--- app/components/student_table_form.rb
+++ app/components/student_table_form.rb
@@ -2,8 +2,8 @@ class StudentTableForm < ViewComponent::Base
   erb_template <<~ERB
     <%= form_with url: url, model: @student, id: dom_id(@student), class: form_class do |form| %>
         <% unless @is_edit%>
-          <% { t(:mlid) => true, t(:last_name) => false, t(:first_name) => false, t(:gender) => false, t(:date_of_birth) => true, t(:enrolled_since) => true, 'Submit' => false }.each do |name, numeric| %>
-            <%= render TableComponents::UnorderedColumn.new(column: { column_name: name, numeric: numeric }) %>
+          <% columns.each do |c| %>
+            <%= render TableComponents::UnorderedColumn.new(column: { column_name: c[:column_name], numeric: c[:numeric], column_span: c[:column_span] }) %>
           <% end %>
         <% end %>
         <div class="table-cell" data-controller="mlid" data-mlid-student-id-value="<%= @student.id %>">
@@ -19,21 +19,21 @@ class StudentTableForm < ViewComponent::Base
           <%= form.text_field :first_name, class: "w-full rounded-md border-purple-500 shadow-xs focus:border-green-600 focus:ring-green-600 sm:text-sm" %>
           <%= render ValidationErrorComponent.new(model: @student, key: :first_name) %>
         </div>
-        <div class="table-cell text-right">
-          <div class="inline-flex items-center h-full">
+        <div class="table-cell flex! items-center gap-1 justify-end">
+          <div class="inline-flex items-center gap-1">
             <%= form.radio_button :gender, :M, class: 'h-4 w-4 border-purple-500 text-green-600 focus:ring-green-600 cursor-pointer', checked: @student.gender == 'M' || @student.gender.nil? %>
-            <label class="ml-1 text-sm font-medium text-gray-700 cursor-pointer"><%= t(:enums)[:student][:gender][:M] %></label>
+            <label class="text-sm font-medium text-gray-700 cursor-pointer"><%= t(:enums)[:student][:gender][:M] %></label>
           </div>
-          <div class="inline-flex items-center h-full">
+          <div class="inline-flex items-center gap-1">
             <%= form.radio_button :gender, :F, class: 'h-4 w-4 border-purple-500 text-green-600 focus:ring-green-600 cursor-pointer', checked: @student.gender == 'F' %>
-            <label class="ml-1 text-sm font-medium text-gray-700 cursor-pointer"><%= t(:enums)[:student][:gender][:F] %></label>
+            <label class="text-sm font-medium text-gray-700 cursor-pointer"><%= t(:enums)[:student][:gender][:F] %></label>
           </div>
-          <div class="inline-flex items-center h-full">
+          <div class="inline-flex items-center gap-1">
             <%= form.radio_button :gender, :NB, class: 'h-4 w-4 border-purple-500 text-green-600 focus:ring-green-600 cursor-pointer', checked: @student.gender == 'NB' %>
-            <label class="ml-1 text-sm font-medium text-gray-700 cursor-pointer"><%= t(:enums)[:student][:gender][:NB] %></label>
+            <label class="text-sm font-medium text-gray-700 cursor-pointer"><%= t(:enums)[:student][:gender][:NB] %></label>
           </div>
         </div>
-        <div class="table-cell flex justify-start gap-2 items-center">
+        <div class="table-cell <%= @is_edit ? '' : 'col-span-2' %>">
           <%= render Datepicker.new(date: @student.dob || Date.current, target: 'dob', form: form) %>
           <%= form.check_box :estimated_dob, checked: @student.estimated_dob , class: 'h-4 w-4 border-purple-500 text-green-600 focus:ring-green-600 cursor-pointer ml-1' %>
           <label class="text-xs font-medium text-gray-700 cursor-pointer"><%= t(:dob_estimated) %></label>
@@ -79,7 +79,12 @@ class StudentTableForm < ViewComponent::Base
     if @is_edit
       'table-row-wrapper student-row'
     else
-      'w-full bg-gray-50 border-b border-gray-200 grid grid-cols-7 justify-between'
+      'w-full bg-gray-50 border-b border-gray-200 grid grid-cols-8 justify-between'
     end
+  end
+
+  def columns
+    [{ column_name: t(:mlid), numeric: true }, { column_name: t(:first_name), numeric: false }, { column_name: t(:last_name), numeric: false }, { column_name: t(:gender), numeric: false },
+     { column_name: t(:date_of_birth), numeric: true, column_span: 2 }, { column_name: t(:enrolled_since), numeric: true }, { column_name: t(:submit), numeric: false }]
   end
 end
--- app/components/table_components/column.rb
+++ app/components/table_components/column.rb
@@ -31,5 +31,6 @@ class TableComponents::UnorderedColumn < TableComponents::Column
   def initialize(column:)
     @column_name = column[:column_name]
     @numeric = column[:numeric]
+    @column_span = column[:column_span]
   end
 end
