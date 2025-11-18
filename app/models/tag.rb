8ac1b699549314aaa287ad1ac38cb6701ae03554
KralMarko123 <kralmarko123@gmail.com>
2025-11-18 22:55:05 +0100

    feat: added shared_organization_ids to tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,12 +2,13 @@
 #
 # Table name: tags
 #
-#  id              :uuid             not null, primary key
-#  shared          :boolean          not null
-#  tag_name        :string           not null
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#  organization_id :bigint           not null
+#  id                      :uuid             not null, primary key
+#  shared                  :boolean          not null
+#  shared_organization_ids :bigint           default([]), is an Array
+#  tag_name                :string           not null
+#  created_at              :datetime         not null
+#  updated_at              :datetime         not null
+#  organization_id         :bigint           not null
 #
 # Indexes
 #
@@ -25,6 +26,16 @@ class Tag < ApplicationRecord
       "Tag \"#{data[:value]}\" already exists in #{Organization.find(tag.organization_id).organization_name} organization"
     end
   }
+  validate :no_duplicate_shared_organization_ids
+  validate :shared_organization_ids_exist
+
+  def no_duplicate_shared_organization_ids
+    errors.add(:shared_organization_ids, 'must contain only unique values') if shared_organization_ids.any? && shared_organization_ids.uniq.length != shared_organization_ids.length
+  end
+
+  def shared_organization_ids_exist
+    errors.add(:shared_organization_ids, 'Organization selected must exist') if shared_organization_ids.any? { |id| !Organization.exists?(id) }
+  end
 
   def can_delete?
     students.none?
