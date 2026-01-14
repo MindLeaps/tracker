1ccf83a4204bfe7fbbb5118fa2ab2b4f3589b87f
KralMarko123 <kralmarko123@gmail.com>
2026-01-14 11:29:45 +0100

    feat: update analytics stimulus controller to filter multiselect component as well

--- app/javascript/controllers/analytics_filter_controller.js
+++ app/javascript/controllers/analytics_filter_controller.js
@@ -11,17 +11,41 @@ function createOption(label, value) {
 }
 
 export default class extends Controller {
-  static targets = ['select', 'anchor']
+  static targets = ['select', 'anchor', 'multiselect']
 
   connect() {
     this.updateFilter()
   }
 
   updateFilter() {
+    // filter dropdowns
     this.updateDropdown(this.selectTargets[0], JSON.parse(this.selectTargets[0].dataset.resources))
-    this.anchorTarget.href = this.selectTargets.reduce((acc, e) => {
-      return acc + e.getAttribute('data-name') + '=' + this.toId(e.value) + '&'
-    }, '?')
+
+    // filter multiselect dropdowns
+    this.updateMultiselectOptions()
+
+    // update anchor link
+    this.updateAnchor()
+  }
+
+  updateAnchor() {
+    const params = new URLSearchParams()
+
+    // add select params
+    this.selectTargets.forEach(e => {
+      const value = this.toId(e.value)
+      params.set(e.getAttribute('data-name'), value)
+    })
+
+    // add multiselect params (hidden inputs)
+    this.multiselectTargets.forEach(wrapper => {
+      const inputs = wrapper.querySelectorAll('input[type="hidden"]')
+      inputs.forEach(i => {
+        if (i.value !== '') params.append(i.name, i.value)
+      })
+    })
+
+    this.anchorTarget.href = `?${params.toString()}`
   }
 
   toId(value) {
@@ -57,5 +81,23 @@ export default class extends Controller {
         this.updateDropdown(targetSelect, JSON.parse(targetSelect.dataset.resources), valueExists ? [currentValue] : filteredIds)
       })
     }
+  }
+
+  updateMultiselectOptions() {
+    // your selects are: organization_id, chapter_id
+    const chapterSelect = this.selectTargets.find(t => t.dataset.name === "chapter_id")
+    const chapterId = this.toId(chapterSelect?.value)
+
+    // Parent ids used for filtering multi selectable groups:
+    // - if chapter selected => filter by chapter
+    const parentIds = chapterId ? [chapterId] : []
+
+    this.multiselectTargets.forEach(wrapper => {
+      const el = wrapper.querySelector('[data-controller~="multiselect"]')
+      if (!el) return
+
+      const controller = this.application.getControllerForElementAndIdentifier(el, "multiselect")
+      controller?.filterOptions(parentIds)
+    })
   }
 }
