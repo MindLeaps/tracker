b76ccbaa305786cf01dbe6533f54acb38d26d9de
KralMarko123 <kralmarko123@gmail.com>
2026-02-20 09:48:10 +0100

    fix: small visual fix

--- app/components/group_enrolled_students_component.rb
+++ app/components/group_enrolled_students_component.rb
@@ -9,7 +9,7 @@ class GroupEnrolledStudentsComponent < ViewComponent::Base
           <%= render TableComponents::Table.new(pagy: @pagy, options: { no_pagination: true, turbo_id: 'students' }, rows: @students, row_component: TableComponents::StudentTurboRow,
            row_arguments: { group: @group, students_with_invalid_grades: @students_with_invalid_grades }) %>
         <% else %>
-          <span class="w-full p-2 text-center bg-gray-50 border-b border-gray-200 text-md font-medium text-gray-500 uppercase tracking-wider">No students enrolled yet</span>
+          <div class="p-2 text-center bg-gray-50 border-b border-gray-200 text-md font-medium text-gray-500 uppercase tracking-wider">No students enrolled yet</div>
         <% end %>
       <% end %>
     <% end %>
