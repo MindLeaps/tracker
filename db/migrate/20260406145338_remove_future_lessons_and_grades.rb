ffcb012ac2cb707a6646a7a553283537051563ee
KralMarko123 <kralmarko123@gmail.com>
2026-04-13 17:30:37 +0200

    feat: modified migration to hard delete future lessons and grades

--- db/migrate/20260406145338_remove_future_lessons_and_grades.rb
+++ db/migrate/20260406145338_remove_future_lessons_and_grades.rb
@@ -1,18 +1,15 @@
 class RemoveFutureLessonsAndGrades < ActiveRecord::Migration[7.2]
-  # rubocop:disable Rails/SkipsModelValidations
   def up
-    deleted_at = Time.zone.now
     today = Time.zone.today
 
     transaction do
-      lessons_to_delete = Lesson.where(deleted_at: nil).where('date > ?', today)
-      grades_to_delete = Grade.where(deleted_at: nil, lesson_id: lessons_to_delete.select(:id))
+      lessons_to_delete = Lesson.where('date > ?', today)
+      grades_to_delete = Grade.where(lesson_id: lessons_to_delete.select(:id))
 
-      grades_to_delete.update_all(deleted_at: deleted_at)
-      lessons_to_delete.update_all(deleted_at: deleted_at)
+      grades_to_delete.delete_all
+      lessons_to_delete.delete_all
     end
   end
-  # rubocop:enable Rails/SkipsModelValidations
 
   def down
     raise ActiveRecord::IrreversibleMigration, 'Cannot safely restore future lessons and grades'
