e431fb0f7ad6ef2bbbb5bbc39a64766bd6f369c1
KralMarko123 <kralmarko123@gmail.com>
2026-04-20 10:59:19 +0200

    spec: fix failing specs

--- spec/controllers/api/lessons_controller_spec.rb
+++ spec/controllers/api/lessons_controller_spec.rb
@@ -44,13 +44,38 @@ RSpec.describe Api::LessonsController, type: :controller do
       end
     end
 
-    context 'lesson already exists' do
+    context 'lesson already exists with the same ID' do
       before :each do
-        create :lesson, group_id: @group.id, subject_id: @subject.id, date: Time.zone.today.to_fs
-        post :create, format: :json, params: { group_id: @group.id, subject_id: @subject.id, date: Time.zone.today.to_fs }
+        @existing_lesson = create :lesson, group_id: @group.id, subject_id: @subject.id, date: Time.zone.today.to_fs
+        post :create, format: :json, params: { id: @existing_lesson.id, group_id: @group.id, subject_id: @subject.id, date: Time.zone.today.to_fs }
       end
 
       it { should respond_with 200 }
+
+      it 'responds with the existing lesson' do
+        expect(lesson['id']).to eq @existing_lesson.id
+      end
+    end
+
+    context 'lesson already exists with a different ID' do
+      before :each do
+        @existing_lesson = create :lesson, group_id: @group.id, subject_id: @subject.id, date: Time.zone.today.to_fs
+        post :create, format: :json, params: { id: 'bbbe6756-1737-4660-b7e5-e95ae0600124', group_id: @group.id, subject_id: @subject.id, date: Time.zone.today.to_fs }
+      end
+
+      it { should respond_with :conflict }
+
+      it 'responds with the conflicting lesson' do
+        expect(lesson['id']).to eq @existing_lesson.id
+      end
+    end
+
+    context 'lesson cannot be created for a non-duplicate reason' do
+      before :each do
+        post :create, format: :json, params: { id: 'bbbe6756-1737-4660-b7e5-e95ae0600124', group_id: @group.id, date: Time.zone.today.to_fs }
+      end
+
+      it { should respond_with :unprocessable_content }
     end
   end
 end
