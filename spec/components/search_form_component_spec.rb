55ca0bb6e82b53b107ea72363c32b89bee49f54e
Kevin Rukundo <kevinrukundo1@gmail.com>
2026-06-25 15:49:15 +0200

    feat(2603):add a clear button on the search component

--- spec/components/search_form_component_spec.rb
+++ spec/components/search_form_component_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+﻿require 'rails_helper'
 
 RSpec.describe SearchFormComponent, type: :component do
   it 'renders query parameters as hidden input fields' do
@@ -23,5 +23,43 @@ RSpec.describe SearchFormComponent, type: :component do
     expect(result.css('input[type=hidden][name=foo][value=bar]').length).to eq 1
     expect(result.css('input[type=hidden][name=table_order\[key\]][value=last_name]').length).to eq 1
     expect(result.css('input[type=hidden][name=table_order\[order\]][value=asc]').length).to eq 1
+  end
+
+  describe 'clear button' do
+    it 'is hidden when the search term is empty' do
+      result = render_inline(SearchFormComponent.new(search: { term: '' }, query_parameters: {}))
+
+      expect(result.css('button[data-autosubmit-target="clearButton"].hidden').length).to eq 1
+    end
+
+    it 'is hidden when the search term is nil' do
+      result = render_inline(SearchFormComponent.new(search: { term: nil }, query_parameters: {}))
+
+      expect(result.css('button[data-autosubmit-target="clearButton"].hidden').length).to eq 1
+    end
+
+    it 'is visible when the search term is present' do
+      result = render_inline(SearchFormComponent.new(search: { term: 'Rwanda' }, query_parameters: {}))
+
+      expect(result.css('button[data-autosubmit-target="clearButton"]:not(.hidden)').length).to eq 1
+    end
+
+    it 'has an aria-label for accessibility' do
+      result = render_inline(SearchFormComponent.new(search: { term: '' }, query_parameters: {}))
+
+      expect(result.css('button[data-autosubmit-target="clearButton"]').first['aria-label']).to be_present
+    end
+
+    it 'is wired to the clear action' do
+      result = render_inline(SearchFormComponent.new(search: { term: '' }, query_parameters: {}))
+
+      expect(result.css('button[data-action="click->autosubmit#clear"]').length).to eq 1
+    end
+
+    it 'the search input is wired to updateClearButton on input' do
+      result = render_inline(SearchFormComponent.new(search: { term: '' }, query_parameters: {}))
+
+      expect(result.css('#search-field').first['data-action']).to include('input->autosubmit#updateClearButton')
+    end
   end
 end
